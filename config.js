fa5d8773be0e0352b5c7c8afcaf1dffe91d23b8c
Sinh0107 <86763182+Sinh0107@users.noreply.github.com>
2021-07-01 20:08:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu anh yêu nhiều <33',
-    btnNo: 'Yêu anh :3',
+    btnNo: 'Yêu anh nhiều nhiều nhiều nhiều luôn :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Bởi vì anh đẹp trai vs dễ thw lắm luôn',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 5h30 chỗ cũ nhé công chúa.',
+    messDesc: 'Thi xong đi ăn nhé công chúa.',
     btnAccept: 'Okiiiii lun <3'.
 }
