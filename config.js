de3d3ebb39e7fb4e13351064bffd7d14ab4174f8
Sinh0107 <86763182+Sinh0107@users.noreply.github.com>
2021-07-02 23:19:26 +0700

    Sinh.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     btnReply: 'Gửi cho thằng Sinh',
     reply: 'khi nãy t chọn 2 loại mà hắn cứ nhảy nhảy nên t mới chọn 3 đó chứ đâu thể sai ngu v đc ',
     mess: 't biết mà hahaha, rảnh quá nên tập học code giết thoèi gian,',
-    messDesc: 'thấy thú vị thì học choei ',
+    messDesc: 'thấy thú vị thì rảnh làm chơi dễ lắm!! ',
     btnAccept: 'Ok',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
